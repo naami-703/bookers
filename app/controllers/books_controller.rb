ece877f9ac3a0c2d51a79527b9348168e5f70316
naami-703 <192409540+naami-703@users.noreply.github.com>
2025-01-11 11:02:16 +0000

    [Finish]課題完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,24 +1,46 @@
 class BooksController < ApplicationController
   def index
     @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+     flash[:notice] = "Book was successfully update"
+     redirect_to book_path(@book.id)
+    else
+      flash[:error] = @book.errors.full_messages.join(",")
+      @error_count = @book.errors.count
+      render :edit
   end
+end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books'
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created"
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:error] = @book.errors.full_messages.join(",")
+      @error_count = @book.errors.count
+      @books = Book.all
+      render :index
+     end
   end
 
   private
